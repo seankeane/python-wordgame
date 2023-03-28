85c1e21d15f461917620faff5ec176798428927e
Sean Keane <seankeane@live.ie>
2023-03-28 19:27:28 +0100

    Add unit tests

--- test.py
+++ test.py
@@ -1,9 +1,24 @@
 import unittest
-from main import check_only_alpha
-
+import wordgame
 
 class MainTest(unittest.TestCase):
 
     def test_check_only_alpha(self):
-        result = check_only_alpha("01234")
-        self.assertEqual(result, False)
+        self.assertEqual(wordgame.check_only_alpha("01234"), False)
+
+    def test_make_wordlist(self):
+        words = wordgame.make_wordlist()
+        # check words that are 5-letters are present
+        self.assertIn("hello", words, "hello is not present")
+        self.assertIn("queen", words, "queen is not present")
+        self.assertIn("field", words, "field is not present")
+        # check words that are not 5-letters are no present
+        self.assertNotIn("fly", words, "fly is present")
+        self.assertNotIn("letter", words, "letter is present")
+        # check if nonsense 5-letter string is presnt
+        self.assertNotIn("xlkjk", words, "xlkjk is present")
+
+    def test_check_is_solved(self):
+        self.assertTrue(wordgame.check_is_solved("hello", "hello"))
+        self.assertFalse(wordgame.check_is_solved("hello", "field"))
+
